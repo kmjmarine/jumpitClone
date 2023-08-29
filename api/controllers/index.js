0c1b07e2cd373a7fde2a0582a675457ce9af9eb8
kmjmarine <kmjmarine@naver.com>
2023-08-29 15:13:43 +0900

    [MODIFY] resolve conflict

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,7 +1,9 @@
 const userController = require("./userController");
 const resumeController = require("./resumeController");
+const uploadController = require("./uploadController");
 
 module.exports = {
   userController,
   resumeController,
+  uploadController,
 };
--- api/services/resumeService.js
+++ api/services/resumeService.js
@@ -4,9 +4,17 @@ const getAllResumes = async (user) => {
   return await resumeDao.getAllResumes(user.id);
 };
 
-const createResume = async (userId, display, githubUrl, notionUrl, blogUrl) => {
+const createResume = async (
+  userId,
+  title,
+  display,
+  githubUrl,
+  notionUrl,
+  blogUrl
+) => {
   return await resumeDao.createResume(
     userId,
+    title,
     display,
     githubUrl,
     notionUrl,
@@ -14,12 +22,42 @@ const createResume = async (userId, display, githubUrl, notionUrl, blogUrl) => {
   );
 };
 
+const getSingleResume = async (user, resumeId) => {
+  return await resumeDao.getSingleResume(user.id, resumeId);
+};
+
 const createEducation = async (resumeId, educations) => {
   return await resumeDao.createEducation(resumeId, educations);
 };
 
+const createCareer = async (resumeId, careers) => {
+  return await resumeDao.createCareer(resumeId, careers);
+};
+
+const createProject = async (resumeId, projects) => {
+  return await resumeDao.createProject(resumeId, projects);
+};
+
+const createAddFile = async (resumeId, addfiles) => {
+  return await resumeDao.createAddFile(resumeId, addfiles);
+};
+
+const deleteResume = async (userId, resumeId) => {
+  return await resumeDao.deleteResume(userId, resumeId);
+};
+
+const deleteAddFile = async (resumeId, addfiles) => {
+  return await resumeDao.deleteAddFile(resumeId, addfiles);
+};
+
 module.exports = {
   getAllResumes,
+  getSingleResume,
   createResume,
   createEducation,
+  createCareer,
+  createProject,
+  createAddFile,
+  deleteResume,
+  deleteAddFile,
 };
